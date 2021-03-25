518e261b6ae0203a0c13024f387f54da5d691c51
Hiten Soni <hso@ex2india.com>
2021-03-25 14:22:28 +0530

    update: showMore directive added

---
+++ src/app/directives/read-more.directive.ts
@@ -0,0 +1,36 @@
+import {
+  Directive,
+  ElementRef,
+  HostListener,
+  Input,
+  AfterViewInit,
+} from '@angular/core';
+
+@Directive({
+  selector: '[appReadMore]',
+})
+export class ReadMoreDirective implements AfterViewInit {
+  @Input('appReadMore') showMoreEnabled: any;
+
+  maxLength: number = 10;
+  text: string;
+  textElement: HTMLParagraphElement;
+
+  @HostListener('click', ['$event']) private fun(e: any) {
+    if (e.target.classList.contains('showMoreClass')) {
+      if (this.showMoreEnabled) {
+        this.textElement.innerText = this.text;
+      } else {
+        this.textElement.innerText =
+          this.text.substr(0, this.maxLength) + '...';
+      }
+    }
+  }
+  constructor(private elRef: ElementRef) {}
+
+  ngAfterViewInit() {
+    this.textElement = this.elRef.nativeElement.children[0];
+    this.text = this.elRef.nativeElement.children[0].innerText;
+    this.textElement.innerText = this.text.substr(0, this.maxLength) + '...';
+  }
+}
