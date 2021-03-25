fad15a21c33913962f2fb9d3b09ef8b75b2b80eb
Hiten Soni <hso@ex2india.com>
2021-03-25 14:23:10 +0530

    update: pipe for currency added

---
+++ src/app/pipes/currency.pipe.ts
@@ -0,0 +1,16 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'currency',
+})
+export class CurrencyPipe implements PipeTransform {
+  currencyMap: { [key: string]: string } = {
+    INR: '₹',
+    USD: '$',
+    EUR: '€',
+    JPY: '¥',
+  };
+  transform(value: number, currName: string): string {
+    return this.currencyMap[currName] + ' ' + value;
+  }
+}
